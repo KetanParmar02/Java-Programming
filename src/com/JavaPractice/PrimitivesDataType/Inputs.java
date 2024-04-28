122655b0e32ad547f5891d985e8e37f2e45f09a6
Ketan Parmar <ketan992132@gmail.com>
2024-04-28 11:59:16 +0530

    First Program

---
+++ src/com/JavaPractice/PrimitivesDataType/Inputs.java
@@ -0,0 +1,12 @@
+package com.JavaPractice.PrimitivesDataType;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Your RollNo. :-  ");
+        int roll_no = in.nextInt();
+        System.out.println("Your RollNo is = "+roll_no);
+    }
+}
---
+++ src/com/JavaPractice/PrimitivesDataType/Primitives.java
@@ -0,0 +1,15 @@
+package com.JavaPractice.PrimitivesDataType;
+
+public class Primitives {
+    public static void main(String[] args) {
+//        Primitives Data Type Example
+        int num = 45;
+        float marks = 567.437f;
+        double number = 48437.743982;
+        long numb = 4637863543L;
+        char ch = 'a';
+        boolean value = true;
+
+        System.out.println(num+" "+marks+" "+number+" "+numb+" "+ch+" "+value);
+    }
+}
