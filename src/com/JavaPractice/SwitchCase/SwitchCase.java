ef7dc6790f168e157585ee22698368c587af5492
Ketan Parmar <ketan992132@gmail.com>
2024-06-01 08:42:49 +0530

    Switch case example

--- src/com/JavaPractice/SwitchCase/SwitchCase.java
+++ src/com/JavaPractice/SwitchCase/SwitchCase.java
@@ -66,9 +66,12 @@ public class SwitchCase {
         // Enhance switch statement example
         int day2 = 1;
         switch (day2) {
-            case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
+            case 1, 2, 3, 4, 5 -> System.   out.println("Weekday");
             case 6, 7 -> System.out.println("Weekend");
             default -> System.out.println("Invalid Day");
         }
+
+        //write Binary tree program
+
     }
 }
