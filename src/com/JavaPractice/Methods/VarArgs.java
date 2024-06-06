7a193d350b8cb8c258e5c14eefb09d1d6089ec19
Ketan Parmar <ketan992132@gmail.com>
2024-06-06 22:36:59 +0530

    Methods Example

--- src/com/JavaPractice/Methods/VarArgs.java
+++ src/com/JavaPractice/Methods/VarArgs.java
@@ -7,6 +7,7 @@ public class VarArgs {
         sum(1, 2, 3, 4, 5);
         sum();
         multipleParameters(5, "Hello", "World", "Java");
+        multipleParameters("Hello", "World", "Java");
     }
 
     static void sum(int... arr) {
@@ -20,6 +21,10 @@ public class VarArgs {
     // multiple parameters
     static void multipleParameters(int a, String... values) {
         System.out.println(a);
+        System.out.println(Arrays.toString(values));
+    }
+
+    static void multipleParameters(String... values) {
         System.out.println(Arrays.toString(values));
     }
 }
